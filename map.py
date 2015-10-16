f4e90e81e56e1901dd77aa575233b9e637aa8151
Marius Kriegerowski <marius.kriegerowski@zmaw.de>
2015-10-16 11:46:17 +0200

    map: guts

--- map.py
+++ map.py
@@ -2,6 +2,7 @@ import numpy as num
 from pyrocko import automap
 import math
 from pyrocko import model, gmtpy
+from pyrocko.guts import Object, Float, List, String
 from pyrocko.moment_tensor import magnitude_to_moment as mag2mom
 from pyrocko.moment_tensor import to6
 
@@ -10,108 +11,86 @@ def uniso(m):
     mc = num.matrix([[xx, 0., 0.],[0., xx, 0.], [0., 0., xx]])
     return m - mc
 
-width = 20.
-height = 18.
-lat = 45.
-lon = 8
-radius = 1000000
-outfn = 'map.pdf'
-stations_fn = 'GERES/array_center.pf'
-station_label_mapping = ['GERES']
-events_fn = 'castor_event_IGN.dat'
-size_factor = 9.
+#width = 20.
+#height = 18.
+#lat = 45.
+#lon = 8
+#radius = 1000000
+#outfn = 'map.pdf'
+#stations_fn = 'GERES/array_center.pf'
+#station_label_mapping = ['GERES']
+#events_fn = 'castor_event_IGN.dat'
+#size_factor = 9.
 
 
+class MapParameters(Object):
+    width = Float.T(help='width of map', optional=True, default=20)
+    height = Float.T(help='heigth of map', optional=True, default=16)
+    lat = Float.T(help='center latitude')
+    lon = Float.T(help='center longitude')
+    radius = Float.T(help='radius of map')
+    outfn = Float.T(help='output filename', optional=True, default='map.png')
+    stations = List.T(model.Station.T(), optional=True)
+    stations_label_mapping = List.T(String.T(), optional=True, default=False)
+    events = List.T(model.Event.T(), optional=True)
 
 
-map = automap.Map(
-    width=width,
-    height=height,
-    lat=lat,
-    lon=lon,
-    radius=radius,
-    topo_resolution_max=200,
-    topo_resolution_min=40.,
-    show_topo=True,
-    show_grid=True,
-    illuminate=True,
-    illuminate_factor_land=0.5,
-    illuminate_factor_ocean=0.25)
+def make_map(lat, lon, radius, outfn, stations=None, events=None, stations_label_mapping=None, map_parameters=None):
+    map = automap.Map(
+        width=width,
+        height=height,
+        lat=lat,
+        lon=lon,
+        radius=radius,
+        topo_resolution_max=200,
+        topo_resolution_min=40.,
+        show_topo=True,
+        show_grid=True,
+        illuminate=True,
+        illuminate_factor_land=0.5,
+        illuminate_factor_ocean=0.25)
 
-map.draw_cities()
+    map.draw_cities()
 
-if stations_fn:
-    stations = model.load_stations(stations_fn)
-    lats = [s.lat for s in stations]
-    lons = [s.lon for s in stations]
+    if stations:
+        lats = [s.lat for s in stations]
+        lons = [s.lon for s in stations]
 
-    map.gmt.psxy(
-        in_columns=(lons, lats),
-        S='t10p',
-        G='black',
-        *map.jxyr)
+        map.gmt.psxy(
+            in_columns=(lons, lats),
+            S='t10p',
+            G='black',
+            *map.jxyr)
 
-    for i_station, s in enumerate(stations):
-        if station_label_mapping:
-            label = station_label_mapping[i_station]
-        else:
-            label = "%s"%s.station
-        map.add_label(s.lat, s.lon, label)
+        for i_station, s in enumerate(stations):
+            if station_label_mapping:
+                label = station_label_mapping[i_station]
+            else:
+                label = "%s"%s.station
+            map.add_label(s.lat, s.lon, label)
 
-if events_fn:
-    events = model.load_events(events_fn)
-    for e in events:
-        if e.moment_tensor:
-            size_cm = math.sqrt(math.sqrt(mag2mom(e.magnitude) / 100e17)) * size_factor
-            m = e.moment_tensor
-            #mc = uniso(m)
-            #mc = mc / e.moment_tensor.scalar_moment() * mag2mom(5.0)
-            #m6 = to6(c)
-            m6 = m.m6_up_south_east()
-            #data = (e.lon), (e.lat) + (10,) + m6 + (1, 0, 0)
-            #data = lonlat_to_en1_km(e.lon, e.lat) + (10,) + m6 + (1, 0, 0)
-            print m6
-            #data = [e.lon, e.lat, 5, m6, 1,0,0]
-            data = (e.lon, e.lat, 10, m.strike1, m.dip1, m.rake1, 1, 0, 0, 'M %1.1f' % e.magnitude)
-            if True:
-                map.gmt.psmeca(
-                    S='%s%g' % ('a', size_cm*2.0),
-                    #G=gmtpy.color(colors[e.cluster]),
-                    G='red',
-                    #W='thinnest,%i/%i/%i' % darken(gmtpy.color_tup(colors[e.cluster])),
-                    #L='thinnest,%i/%i/%i' % darken(gmtpy.color_tup(colors[e.cluster])),
-                    in_rows=[data],
-                    *map.jxyr)
+    if events:
+        for e in events:
+            if e.moment_tensor:
+                size_cm = math.sqrt(math.sqrt(mag2mom(e.magnitude) / 100e17)) * size_factor
+                m = e.moment_tensor
+                #mc = uniso(m)
+                #mc = mc / e.moment_tensor.scalar_moment() * mag2mom(5.0)
+                #m6 = to6(c)
+                m6 = m.m6_up_south_east()
+                #data = (e.lon), (e.lat) + (10,) + m6 + (1, 0, 0)
+                #data = lonlat_to_en1_km(e.lon, e.lat) + (10,) + m6 + (1, 0, 0)
+                #data = [e.lon, e.lat, 5, m6, 1,0,0]
+                data = (e.lon, e.lat, 10, m.strike1, m.dip1, m.rake1, 1, 0, 0, 'M %1.1f' % e.magnitude)
+                if True:
+                    map.gmt.psmeca(
+                        S='%s%g' % ('a', size_cm*2.0),
+                        #G=gmtpy.color(colors[e.cluster]),
+                        G='red',
+                        #W='thinnest,%i/%i/%i' % darken(gmtpy.color_tup(colors[e.cluster])),
+                        #L='thinnest,%i/%i/%i' % darken(gmtpy.color_tup(colors[e.cluster])),
+                        in_rows=[data],
+                        *map.jxyr)
 
-
-
-    #lats = [e.lat for e in events]
-    #lons = [e.lon for e in events]
-
-    #depths = [e.depth for e in events]
-    #strikes = [e.moment_tensor.strike1 for e in events]
-    #dips = [e.moment_tensor.dip1 for e in events]
-    #rakes = [e.moment_tensor.rake1 for e in events]
-    #mags = [e.moment_tensor.magnitude for e in events]
-    #newx = [0] * len(events)
-    #newy = [0] * len(events)
-    #titles = [''] * len(events)
-    #print titles
-    #print newx 
-    #print newy 
-    #print mags
-    #print rakes 
-
-    #map.gmt.psmeca(
-    #    in_columns=(lons, lats, depths, strikes, dips, rakes, mags, newx, newy, titles),
-    #    #S='a',
-    #    #G='red',
-    #    *map.jxyr)
-    #map.gmt.psxy(
-    #    in_columns=(lons, lats),
-    #    S='a20p',
-    #    G='red',
-    #    *map.jxyr)
-
-map.save(outfn)
+    map.save(outfn)
 
