bfd94a27c113daae9c4649e4759caf01b3a6e31e
Marius Kriegerowski <marius@gfz-potsdam.de>
2015-10-20 15:21:54 +0200

    handle catalogs and set min/max dists

--- store_creator.py
+++ store_creator.py
@@ -8,16 +8,18 @@ from pyrocko.gf.store import Store
 from pyrocko.gf.meta import Timing, ConfigTypeA, TPDef
 from pyrocko import cake
 from util import create_directory
+from collections import defaultdict
 import copy
 import tempfile
 import logging
+
 km = 1000.
 pjoin = os.path.join
 logging.basicConfig(level="INFO")
 logger = logging.getLogger("propose-store")
 
-def propose_store(station, event, superdir, source_depth_min=0., source_depth_max=15.,
-                  source_depth_delta=1., sample_rate=10., force_overwrite=False):
+def propose_store(station, events, superdir, source_depth_min=0., source_depth_max=15.,
+                  source_depth_delta=1., sample_rate=10., force_overwrite=False, numdists=2):
     ''' Propose a fomosto store configuration for P-pP Array beam forming.
     :param event: Event instance
     :param station: Station instance
@@ -28,75 +30,86 @@ def propose_store(station, event, superdir, source_depth_min=0., source_depth_ma
     :param sample_rate: in Hz
     :param force_overwrite: overwrite potentially existent store'''
     modelling_code_id = 'qseis.2006a'
-    distance = ortho.distance_accurate50m(station, event)
-    depth = event.depth
-    config = ConfigTypeA(id='[placeholder]',
+
+    earthmodels_1d = {}
+    dists = defaultdict(list)
+    for e in events:
+        prof = crust2x2.get_profile(e.lat, e.lon)
+        dists[prof._ident].append(ortho.distance_accurate50m(e, station))
+        earthmodels_1d[prof._ident] = prof
+
+    _config = ConfigTypeA(id='[placeholder]',
                          source_depth_min=source_depth_min*km,
                          source_depth_max=source_depth_max*km,
                          source_depth_delta=source_depth_delta*km,
-                         distance_min=0.,
-                         distance_max=0.,
-                         distance_delta=0.,
+                         distance_min=0,
+                         distance_max=0,
+                         distance_delta=0,
                          sample_rate=sample_rate,
                          ncomponents=10)
 
-    config.distance_min = math.floor(distance-5*km)
-    config.distance_max = math.ceil(distance+5*km)
-    config.distance_delta = 10*km
-    configid = ''
-    for item in zip([station, event],['earthmodel_receiver_1d', 'earthmodel_1d']):
-        location, model_id = item
-        if model_id=='earthmodel_receiver_1d':
-            mod = cake.LayeredModel.from_scanlines(
-                cake.from_crust2x2_profile(
-                    crust2x2.get_profile(location.lat, location.lon)))
-            configid += '%s_%s_' % (station.station, crust2x2.get_profile(location.lat, location.lon)._ident)
+    station_crust = crust2x2.get_profile(station.lat, station.lon)
+    mod = cake.LayeredModel.from_scanlines(cake.from_crust2x2_profile(station_crust))
+    setattr(_config, 'earthmodel_receiver_1d', mod)
+
+    configid = '%s_%s_' % (station.station, station_crust._ident)
+    configs = []
+    for ident, prof in earthmodels_1d.items():
+        config = copy.copy(_config)
+
+        config.distance_min = math.floor(min(dists[ident]))
+        config.distance_max = math.ceil(max(dists[ident]))
+        if len(events)==1:
+            distance_delta = 10*km
         else:
-            mod = cake.load_model()
-            mod = mod.replaced_crust((location.lat, location.lon))
-            configid += '%s' % (crust2x2.get_profile(location.lat, location.lon)._ident)
-        setattr(config, model_id, mod)
+            distance_delta = (config.distance_max-config.distance_min)/(numdists-1)
+        config.distance_delta = distance_delta
+        mod = cake.load_model(crust2_profile=prof)
+        configid += '%s' % ident
+        setattr(config, 'earthmodel_1d', mod)
+        configs.append(_config)
+        config.id = configid
+        dest_dir = pjoin(superdir, config.id) 
+        create_directory(dest_dir, force_overwrite)
+        logger.info('Created store under: %s' % dest_dir)
 
-    config.id = configid
-    dest_dir = pjoin(superdir, config.id) 
-    create_directory(dest_dir, force_overwrite)
-    logger.info('Created store under: %s' % dest_dir)
+        mean_z = num.mean([config.source_depth_min, config.source_depth_max])
 
-    mean_z = num.mean([config.source_depth_min, config.source_depth_max])
+        mean_dist = config.distance_min+(config.distance_max-config.distance_min)/2.
+        arrivals = config.earthmodel_1d.arrivals(phases=cake.PhaseDef('P'),
+                                                 distances=[mean_dist*cake.m2d],
+                                                 zstart=mean_z)
+        if len(arrivals)==0:
+            slow = 0.1
+        else:
+            slow = arrivals[0].p/(cake.r2d*cake.d2m/km)
 
+        config.modelling_code_id=modelling_code_id
+        config.tabulated_phases=[
+            TPDef(
+                id='begin',
+                definition='P,P\\,Pv_(cmb)p'),
+            TPDef(
+                id='end',
+                definition='2.5'),
+            TPDef(
+                id='P',
+                definition='!P')]
 
-    arrivals = config.earthmodel_1d.arrivals(phases=cake.PhaseDef('P'),
-                                             distances=[distance*cake.m2d],
-                                             zstart=mean_z)
-    if len(arrivals)==0:
-        slow = 0.1
-    else:
-        slow = arrivals[0].p/(cake.r2d*cake.d2m/km)
+        qs = qseis.QSeisConfig()
+        qs.qseis_version = config.modelling_code_id.split('.')[1]
+        half_lapse_time = 70
+        qs.time_region = (Timing('begin-%s' % (half_lapse_time*1.1)), Timing('begin+%s' % (half_lapse_time*1.1)))
+        qs.cut = (Timing('begin-%s' % half_lapse_time), Timing('begin+%s' % half_lapse_time))
+        qs.slowness_window = (0., 0., slow+slow*0.3, slow+slow*0.5)
+        qs.filter_shallow_paths = 1
+        qs.filter_shallow_paths_depth = round(mean_dist/50000.)
+        qs.validate()
+        config.validate()
+        Store.create_editables(dest_dir, config=config, extra={'qseis': qs})
 
-    config.modelling_code_id=modelling_code_id
-    config.tabulated_phases=[
-        TPDef(
-            id='begin',
-            definition='P,P\\,Pv_(cmb)p'),
-        TPDef(
-            id='end',
-            definition='2.5'),
-        TPDef(
-            id='P',
-            definition='!P')]
-
-    qs = qseis.QSeisConfig()
-    qs.qseis_version = config.modelling_code_id.split('.')[1]
-    qs.time_region = (Timing('begin-50'), Timing('begin+100'))
-    qs.cut = (Timing('begin-50'), Timing('begin+100'))
-    qs.slowness_window = (0., 0., slow+slow*0.5, slow+slow*0.8)
-    qs.filter_shallow_paths = 1
-    qs.filter_shallow_paths_depth = round(distance/100000.)
-    qs.validate()
-    config.validate()
-    Store.create_editables(dest_dir, config=config, extra={'qseis': qs})
-
-    return configid
+    config_ids = [c.id for c in configs]
+    return config_ids
 
 
 
