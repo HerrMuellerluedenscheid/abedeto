ebaed496b93b3d48e5361c0e946dc811aef08bc2
Marius Kriegerowski <marius@gfz-potsdam.de>
2016-02-10 12:40:33 +0100

    pep8

--- src/request.py
+++ src/request.py
@@ -1,17 +1,13 @@
 import os
-import shutil
 from collections import defaultdict
 from pyrocko.fdsn import ws, station as fdsnstation
+from pyrocko.gf import store
 from pyrocko import model
 from pyrocko import cake
-from pyrocko import orthodrome as ortho
 from pyrocko import io
 from pyrocko import orthodrome as ortho
-from pyrocko import util as putil
-from pyrocko.gf.meta import Timing as PyrockoTiming
 from pyrocko.guts import Object, String, Float, List, Dict
 import logging
-from util import create_directory
 
 pjoin = os.path.join
 logging.basicConfig(level='INFO')
@@ -28,6 +24,7 @@ class CakeTiming(Object):
     CakeTiming(definition)'''
     phase_selection = String.T()
     fallback_time = Float.T(optional=True)
+
     def __init__(self, phase_selection, fallback_time=None):
         self.arrivals = defaultdict(dict)
         self.fallback_time = fallback_time
@@ -52,7 +49,7 @@ class CakeTiming(Object):
         self.phases = _phase_selection.split('|')
 
     def return_time(self, ray):
-        if ray == None:
+        if ray is None:
             return self.fallback_time
         else:
             return ray.t + self.offset
@@ -66,9 +63,11 @@ class CakeTiming(Object):
             return self.return_time(self.arrivals[(dist, z)])
 
         phases = [cake.PhaseDef(pid) for pid in self.phases]
-        arrivals = mod.arrivals(distances=[dist*cake.m2d], phases=phases, zstart=z)
-        if arrivals==[]:
-            logger.warn('none of defined phases at d=%s, z=%s. (return fallback time)'  % (dist, z))
+        arrivals = mod.arrivals(
+            distances=[dist*cake.m2d], phases=phases, zstart=z)
+        if arrivals == []:
+            logger.warn(
+                'no phase at d=%s, z=%s. (return fallback time)' % (dist, z))
             want = None
         else:
             want = self.phase_selector(arrivals)
@@ -79,9 +78,9 @@ class CakeTiming(Object):
             return self.return_time(want)
 
     def phase_selector(self, _list):
-        if self.which=='first':
+        if self.which == 'first':
             return min(_list, key=lambda x: x.t)
-        if self.which=='last':
+        if self.which == 'last':
             return max(_list, key=lambda x: x.t)
 
     def strip(self, ps):
@@ -90,11 +89,10 @@ class CakeTiming(Object):
         ps = ps.lstrip('(')
         return ps
 
-    def pyrocko_timing(self):
-        return Timing()
 
 class Timings(Object):
     timings = List.T(CakeTiming.T())
+
     def __init__(self, timings):
         self.timings = timings
 
@@ -102,7 +100,7 @@ class Timings(Object):
 class DataProvider(Object):
     use = List.T(String.T())
     timings = Dict.T(String.T(), Timings.T())
-    #receiver_crusts = Dict.T(String.T(), String.T())
+
     def __init__(self, channels='SHZ', use=None, timings=None):
         self.use = use or []
         self.timings = timings or {}
@@ -115,9 +113,9 @@ class DataProvider(Object):
                        'NIA': ('IM', 'I56H?', '', channels),
                        'PFIA': [('IM', 'I57H?', '', channels),
                                 ('IM', 'I57L?', '', channels)],
-                       'BMA' : ('IM', 'BM0?', '', channels),
-                       'BCA' : ('IM', 'BC0?', '', channels),
-                       'HIA':  ('IM', 'I59H?', '', channels),
+                       'BMA': ('IM', 'BM0?', '', channels),
+                       'BCA': ('IM', 'BC0?', '', channels),
+                       'HIA': ('IM', 'I59H?', '', channels),
                        'NVAR': ('IM', 'NV*', '', channels),
                        'PDAR': ('IM', 'PD*', '', channels),
                        'TXAR': ('IM', 'TX*', '', channels),
@@ -127,29 +125,29 @@ class DataProvider(Object):
                                  ('IM', 'GEB?', '', channels),
                                  ('IM', 'GEC?', '', channels),
                                  ('IM', 'GED?', '', channels)],
-                       # Diego Garcia Hydroacoustic array
+                       # Diego Garcia Hydroacoustic array noqa
                        'DGHAland': ('IM', 'I52H?', '', channels),
                        'DGHAS': ('IM', 'H08S?', '', channels),
                        'DGHAN': ('IM', 'H08N?', '', channels),
-                       #Tristan da Cunha. channels: BDF.
+                       # Tristan da Cunha. channels: BDF. noqa
                        'TDC': [('IM', 'H09N?', '', channels),
-                                ('IM', 'I49H?', '', channels)],
-                        'NarroginIA': ('IM', 'I04H?', '', channels),
-                        'CocosIslands': ('IM', 'I06H?', '' , channels),
-                        'Warramunga': ('IM', 'I07H?', '', channels),
-                        'BermudaIA': ('IM', 'I51H?', '', channels),
-                        'FairbanksIA': ('IM', 'I53H?', '', channels)}
+                               ('IM', 'I49H?', '', channels)],
+                       'NarroginIA': ('IM', 'I04H?', '', channels),
+                       'CocosIslands': ('IM', 'I06H?', '', channels),
+                       'Warramunga': ('IM', 'I07H?', '', channels),
+                       'BermudaIA': ('IM', 'I51H?', '', channels),
+                       'FairbanksIA': ('IM', 'I53H?', '', channels)}
 
-        geofon_arrays = { 'ROHRBACH':('6A', 'V*', '', channels),
-                              'AntaOffshore': ('GR', 'I27L?', '*', channels),
-                              'AntaOnshore': ('AW', 'VNA*', '*', channels),
-                              'NORES': [('NO', 'NA*', '*', channels),
-                                       ('NO', 'NB*', '*', channels),
-                                       ('NO', 'NC*', '*', channels)]}
-        bgr_arrays = {'GERES':[('GR', 'GEA?', '*', channels),
-                                     ('GR', 'GEB?', '*', channels),
-                                     ('GR', 'GEC?', '*', channels),
-                                     ('GR', 'GED?', '*', channels)],}
+        geofon_arrays = {'ROHRBACH': ('6A', 'V*', '', channels),
+                         'AntaOffshore': ('GR', 'I27L?', '*', channels),
+                         'AntaOnshore': ('AW', 'VNA*', '*', channels),
+                         'NORES': [('NO', 'NA*', '*', channels),
+                                   ('NO', 'NB*', '*', channels),
+                                   ('NO', 'NC*', '*', channels)]}
+        bgr_arrays = {'GERES': [('GR', 'GEA?', '*', channels),
+                                ('GR', 'GEB?', '*', channels),
+                                ('GR', 'GEC?', '*', channels),
+                                ('GR', 'GED?', '*', channels)], }
 
         self.providers = {'iris': iris_arrays,
                           'geofon': geofon_arrays,
@@ -162,29 +160,31 @@ class DataProvider(Object):
         use = []
         ts = {}
         unit = 'M'
-        if all([timing, length]) == None:
+        if all([timing, length]) is None:
             raise Exception('Define one of "timing" and "length"')
         prefix = prefix or ''
         directory = pjoin(prefix, directory)
-        create_directory(directory, force)
+        store.remake_dir(directory, force)
         pzresponses = {}
         for site, array_data_provder in self.providers.items():
             logger.info('requesting data from site %s' % site)
             for array_id, codes in array_data_provder.items():
-                if array_id not in want and want!='all':
+                if array_id not in want and want != 'all':
                     continue
                 sub_directory = pjoin(directory, array_id)
                 logger.info("fetching %s" % array_id)
                 codes = array_data_provder[array_id]
                 if not isinstance(codes, list):
                     codes = [codes]
-                selection = [c + tuple((event.time, event.time+1000.)) for c in codes]
+                selection = [
+                    c + tuple((event.time, event.time+1000.)) for c in codes]
                 logger.debug('selection: ', selection)
                 try:
                     st = ws.station(site=site, selection=selection)
                 except ws.EmptyResult as e:
-                    logging.error('%s on %s. skip' %(e, array_id))
+                    logging.error('%s on %s. skip' % (e, array_id))
                     continue
+
                 stations = st.get_pyrocko_stations()
                 min_dist = min(
                     [ortho.distance_accurate50m(s, event) for s in stations])
@@ -198,10 +198,12 @@ class DataProvider(Object):
                 elif timing:
                     tstart = timing[0].t(mod, (event.depth, min_dist))
                     tend = timing[1].t(mod, (event.depth, max_dist))
-                selection = [c + tuple((event.time + tstart, event.time + tend)) for c in codes]
+                selection = [
+                    c + tuple((event.time + tstart, event.time + tend)
+                              ) for c in codes]
                 try:
                     d = ws.dataselect(site=site, selection=selection)
-                    create_directory(sub_directory, force)
+                    store.remake_dir(sub_directory, force)
                     fn = pjoin(sub_directory, 'traces.mseed')
                     with open(fn, 'w') as f:
                         f.write(d.read())
@@ -210,16 +212,20 @@ class DataProvider(Object):
                     for tr in trs:
                         try:
                             pzresponse = st.get_pyrocko_response(
-                                nslc=tr.nslc_id, timespan=(tr.tmin, tr.tmax),
+                                nslc=tr.nslc_id,
+                                timespan=(tr.tmin, tr.tmax),
                                 fake_input_units=unit)
-                            pzresponses[tr.nslc_id] = pzresponse
+                            e = pzresponse
                         except fdsnstation.NoResponseInformation as e:
                             logger.warn("no response information: %s" % e)
                             pass
                         except fdsnstation.MultipleResponseInformation as e:
                             logger.warn("MultipleResponseInformation: %s" % e)
                             pass
-                    model.dump_stations(stations, pjoin(sub_directory, 'stations.pf'))
+                        pzresponses[tr.nslc_id] = e
+                    model.dump_stations(
+                        stations, pjoin(sub_directory, 'stations.pf'))
+
                     if dump_config and timing:
                         t = Timings(list(timing))
                         ts[array_id] = t
@@ -227,29 +233,32 @@ class DataProvider(Object):
                     if array_id not in use:
                         use.append(array_id)
                 except ws.EmptyResult as e:
-                    logging.error('%s on %s' %(e, array_id))
+                    logging.error('%s on %s' % (e, array_id))
+
         if dump_config:
             self.timings = ts
             self.use = use
             self.dump(filename=pjoin(prefix, 'request.yaml'))
 
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser(description='download waveforms')
-    parser.add_argument('--events', help='event file. uses only first event in list!')
+    parser.add_argument(
+        '--events', help='event file. uses only first event in list!')
     parser.add_argument('--site', help='geofon|iris', default='iris')
     parser.add_argument('--want', help='all|id1,id2,id3....', default='all')
     args = parser.parse_args()
     length = 1000.
-    print 'use first event in list'
+    print 'use first event in list as test'
     e = list(model.Event.load_catalog(args.events))[0]
     provider = DataProvider(channels='*Z')
     tmin = CakeTiming(phase_selection='first(p|P|PP)-10', fallback_time=0.001)
     tmax = CakeTiming(phase_selection='first(p|P|PP)+52', fallback_time=1000.)
     want = args.want
-    if want!='all':
+    if want != 'all':
         want = want.split(',')
-    provider.download(e, timing=(tmin, tmax), dump_config=True, force=True, want=want)
+
+    provider.download(
+        e, timing=(tmin, tmax), dump_config=True, force=True, want=want)
